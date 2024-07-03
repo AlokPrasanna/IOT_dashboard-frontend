c7b4d718387e98c23c6ef83051cc4ad13e484569
AlokPrasanna <alokprasanna1104@gmail.com>
2024-07-03 13:23:39 +0530

    Updated BaseUrl

--- src/context/BaseUrl/BaseUrlContext.tsx
+++ src/context/BaseUrl/BaseUrlContext.tsx
@@ -8,7 +8,7 @@ interface BaseUrlContextType {
 const BaseUrlContext = createContext<BaseUrlContextType | undefined>(undefined);
 
 export const BaseUrlProvider: React.FC<{children:ReactNode}> = ({children}) => {
-    const [baseUrl , setBaseUrl] = useState<string>("https://iotdashboardbackend-production.up.railway.app/");
+    const [baseUrl , setBaseUrl] = useState<string>("https://iotdashboardbackend-production.up.railway.app/api/");
 
     return (
         <BaseUrlContext.Provider value={{baseUrl , setBaseUrl}}>
