a65908a91cea4cf4ce1fc1b1260f8a4b664df44d
AlokPrasanna <alokprasanna1104@gmail.com>
2024-07-08 13:18:34 +0530

    Remove Warining messages

--- src/components/atoms/Table/Table.tsx
+++ src/components/atoms/Table/Table.tsx
@@ -219,8 +219,8 @@ const Table: React.FC<TableDataProps> = ({ columns, data, onRowSelect , onAction
             </table>
             <div>
                 <button onClick={firstPage} disabled={currentPage === 1} className="pagination-btn" style={{marginRight:"4px"}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5" />
                     </svg>
                 </button>
                 <button onClick={prevPage} disabled={currentPage === 1} className="pagination-btn">
@@ -235,8 +235,8 @@ const Table: React.FC<TableDataProps> = ({ columns, data, onRowSelect , onAction
                     </svg>
                 </button>
                 <button onClick={lastPage} disabled={currentPage === totalPages} className="pagination-btn" style={{marginLeft:"4px"}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                     </svg>
                 </button>
             </div>
